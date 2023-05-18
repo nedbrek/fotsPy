7c9c9e5d5178d96fd19432887c0e8d874d245578
Edward Brekelbaum <nedbrek@yahoo.com>
2023-05-17 22:24:57 -0500

    dump_map.py: Moved functionality into fots_lib.py (new)

---
+++ fots_lib.py
@@ -0,0 +1,189 @@
+import openpyxl
+import sys
+
+### classes
+class World:
+    def __init__(self):
+        self.type = "<error>"
+        self.rp = -1
+        self.srp = -1
+
+class System:
+    def __init__(self):
+        self.xoff = -1
+        self.yoff = -1
+        self.key = "<error>"
+        self.star_type = "<error>"
+        self.worlds = list()
+        self.grid = False
+
+class Habs:
+    def __init__(self):
+        self.habs = dict()
+
+    def readExcel(self, wb_obj):
+        colony_sheet = wb_obj["Colonies"]
+        row = 2
+        while 1:
+            col = 64
+            key_val = colony_sheet.cell(row, col).value
+            if key_val == None:
+                break
+
+            col = 66
+            hab_val = colony_sheet.cell(row, col).value
+            if hab_val == None:
+                print("Error in hab val, row:", row)
+                hab_val = "?"
+
+            self.habs[key_val] = hab_val
+            row = row + 1
+
+class Fots:
+    def __init__(self):
+        self.stars = list()
+
+    # excel util
+    def resolveFormula(sheet, cell):
+        val = cell.value
+        if type(val) is int:
+            return val
+
+        if val[0] == '=':
+            plus_idx = val.find("+")
+            idx = val[1:plus_idx]
+            v2 = sheet[idx].value
+            const = int(val[plus_idx+1:])
+            v2 = v2 + const
+            cell.value = v2
+
+        return v2
+
+    # find x dimensions
+    def findX(map_sheet):
+        row = 2
+        col = 7
+        first_x = map_sheet.cell(row, col).value
+        while 1:
+            cell = map_sheet.cell(row, col)
+            val = cell.value
+            if val == None:
+                break
+
+            last_x = Fots.resolveFormula(map_sheet, cell)
+
+            col = col + 10
+        #end while
+        return (first_x, last_x)
+
+    # find grid y
+    def findY(map_sheet):
+        row = 8
+        col = 1
+        first_y = map_sheet.cell(row, col).value
+        while 1:
+            cell = map_sheet.cell(row, col)
+            val = cell.value
+            if val == None:
+                break
+
+            last_y = Fots.resolveFormula(map_sheet, cell)
+
+            row = row + 10
+        #end while
+        return (first_y, last_y)
+
+    def makeKey(grid_x, grid_y):
+        dec_x = int(grid_x / 10)
+        sec_x = (grid_x % 10) + 1
+
+        dec_y = int(grid_y / 10)
+        sec_y = grid_y % 10
+        key = "{}x{}/{:02d}".format(dec_x, dec_y, sec_x + sec_y * 10)
+        return key
+
+    def parseComments(self, s, comments):
+        last_comment = comments[-1]
+        lc_last_col = last_comment.split()
+        last_survey = -1
+        if (len(lc_last_col) > 2):
+            last_survey = int(lc_last_col[2])
+
+        for c in comments[0:-1]:
+            cols = c.split()
+            num_cols = len(cols)
+            w = World()
+
+            if num_cols == 2:
+                w.type = cols[1]
+            elif num_cols == 6:
+                w.type = cols[2]
+                w.rp = cols[3]
+                w.srp = 0
+                last_survey = cols[4]
+            else:
+                last_col = num_cols - 2
+                last_col_val = cols[last_col]
+                note = None
+                if type(last_col_val) is int or last_col_val.isdigit():
+                    last_survey = last_col_val
+                else:
+                    w.owner = last_col_val
+                    last_col = last_col - 1
+                    last_survey = cols[last_col]
+
+                last_col = last_col - 1
+
+                #num = cols[1]
+                w.type = cols[2]
+                if '.' in cols:
+                    #desc = ' '.join(cols[3:last_col])
+                    last_col = cols.index('.')
+                    last_col = last_col - 1
+                    w.srp = float(cols[last_col])
+                    last_col = last_col - 1
+                else:
+                    w.srp = 0
+
+                w.rp = cols[last_col]
+
+            s.last_survey = last_survey
+            s.worlds.append(w)
+
+    def buildStarmap(self, map_sheet):
+        self.first_x, last_x = Fots.findX(map_sheet)
+        self.first_y, last_y = Fots.findY(map_sheet)
+
+        # walk the map
+        for col in range(3, (last_x - self.first_x + 1) * 10):
+            for row in range(4, (last_y - self.first_y + 1) * 10):
+                cell = map_sheet.cell(row, col)
+                val = cell.value
+                if val == None:
+                    continue
+
+                s = System()
+
+                s.xoff = self.first_x * 10 + col - 3
+                s.yoff = self.first_y * 10 + row - 4
+
+                key = Fots.makeKey(s.xoff, s.yoff)
+                s.key = key
+
+                fill = cell.fill
+                if fill.bgColor.rgb != 'FF000000':
+                    s.grid = True
+
+                comment = cell.comment
+                if type(comment) is not openpyxl.comments.comments.Comment:
+                    s.star_type = val
+                else:
+                    comments = comment.text.split("\n")
+                    com_cols = comments[0].split()
+                    if key != com_cols[0]:
+                        print("Ned error", key, com_cols[0], file=sys.stderr)
+                    s.star_type = com_cols[1]
+                    self.parseComments(s, comments[1:])
+
+                self.stars.append(s)
+
