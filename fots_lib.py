a0f6410c7dc4ddbee6c38e804563282d4cb1f503
Edward Brekelbaum <nedbrek@yahoo.com>
2023-08-07 22:35:44 -0500

    fots_lib.py: Improved comment parsing    Parse all comments now    Parse planet tags    More error checking

--- fots_lib.py
+++ fots_lib.py
@@ -1,4 +1,5 @@
 import openpyxl
+import re
 import sys
 
 ### classes
@@ -109,43 +110,126 @@ class Fots:
         if (len(lc_last_col) > 2):
             last_survey = int(lc_last_col[2])
 
-        for c in comments[0:-1]:
+        for c in comments:
+            regex = re.compile("Special: Wormhole")
+            if regex.search(c):
+                # TODO log wormhole
+                print("NedW {} '{}'".format(s.key, c))
+                continue
+
+            regex = re.compile(": Foreign Fleet")
+            m = regex.search(c)
+            if m:
+                # TODO log foreign fleet
+                c = c[0:m.span()[0]]
+
             cols = c.split()
-            num_cols = len(cols)
+
+            if cols[0] == "Last" and cols[1] == "Survey:":
+                # TODO? check survey stamps?
+                continue
+
+            if cols[1][0] != '#':
+                #print("NedS {} '{}'".format(s.key, c))
+                continue
+
             w = World()
 
+            num_cols = len(cols)
             if num_cols == 2:
                 w.type = cols[1]
+                print("NedC2 {} '{}'".format(s.key, c))
             elif num_cols == 6:
+                #num = cols[1]
                 w.type = cols[2]
                 w.rp = cols[3]
                 w.srp = 0
                 last_survey = int(cols[4])
+                if cols[5] != "!":
+                    print("NedC6 {} '{}'".format(s.key, c))
             else:
+            #{
+                #print("Ned {} '{}'".format(s.key, c))
+                #num = cols[1]
+                w.type = cols[2]
+
+                # parse planet tags
+                i = 3
+                if i >= len(cols):
+                    print("NedD {} '{}'".format(s.key, c))
+                    continue
+
+                if cols[i] == "Waterworld":
+                    # TODO log waterworld
+                    i = i + 1
+
+                if cols[i] == "gas":
+                    # TODO log gas giant
+                    i = i + 1
+                    if cols[i] != "giant":
+                        print("NedG {} '{}'".format(s.key, c))
+                        continue
+                    i = i + 1
+                    if cols[i] == "-":
+                        i = i + 1
+
+                if cols[i] == "asteroids":
+                    # TODO log asteroids
+                    i = i + 1
+
+                if cols[i] == "oort":
+                    # TODO log oort cloud
+                    i = i + 1
+                    if cols[i] != "cloud":
+                        print("NedO {} '{}'".format(s.key, c))
+                        continue
+                    i = i + 1
+
+                if cols[i] == "dextro" or cols[i] == "levo" or cols[i] == "silicon" or cols[i] == "methane":
+                    # TODO log life type
+                    i = i + 1
+                    if cols[i] != "life":
+                        print("NedL {} '{}'".format(s.key, c))
+                        continue
+                    i = i + 1
+
+                # find last column
                 last_col = num_cols - 2
                 last_col_val = cols[last_col]
-                note = None
+
                 if type(last_col_val) is int or last_col_val.isdigit():
                     last_survey = int(last_col_val)
                 else:
                     w.owner = last_col_val
                     last_col = last_col - 1
-                    last_survey = int(cols[last_col])
+                    last_col_val = cols[last_col]
+                    if type(last_col_val) is int or last_col_val.isdigit():
+                        last_survey = int(last_col_val)
 
                 last_col = last_col - 1
 
-                #num = cols[1]
-                w.type = cols[2]
-                if '.' in cols:
+                regex = re.compile("Special: Strategic Resource")
+                if regex.search(c):
+                    # SRP world, TODO capture special resource name
+
                     #desc = ' '.join(cols[3:last_col])
-                    last_col = cols.index('.')
-                    last_col = last_col - 1
+                    last_col = cols.index('Special:') - 2
                     w.srp = float(cols[last_col])
                     last_col = last_col - 1
                 else:
-                    w.srp = 0
+                    regex = re.compile("Naturally")
+                    if (regex.search(c)):
+                        last_col = cols.index('Naturally') - 1
+                        w.srp = float(cols[last_col])
+                        last_col = last_col - 1
+                    else:
+                        w.srp = 0
 
                 w.rp = cols[last_col]
+
+                if i != last_col:
+                    print("Ned4 {} {} {} {} '{}'".format(s.key, i, last_col, w.rp, c))
+            #}
 
             s.last_survey = last_survey
             s.worlds.append(w)
@@ -183,7 +267,7 @@ class Fots:
                     if key != com_cols[0]:
                         print("Ned error ", key, com_cols[0], file=sys.stderr)
                     s.star_type = com_cols[1]
-                    self.parseComments(s, comments[1:])
+                    self.parseComments(s, comments)
 
                 self.stars.append(s)
 
